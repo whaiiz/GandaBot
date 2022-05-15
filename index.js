c6b8a47932a02d689f7f3b5b9a36b7e2db22ba6f
Jaime <jaime_reis18@outlook.pt>
2022-05-15 16:21:30 +0000

    Creation of command logic in index.js

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import { Client, Intents } from "discord.js";
+import { createReminder } from './commands/create-reminder.js';
+import { startDatabaseConnection, stopDatabaseConnection } from './database-connection-manager.js';
 import dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config();
 
 const botToken = process.env.TOKEN;
+const commands = {
+	"create-reminder": createReminder,
+	"stop": stopDatabaseConnection,
+}
 
 const client = new Client({
 	intents: [
@@ -15,14 +21,23 @@ const client = new Client({
 	partials: ['CHANNEL']
 });
 
-client.on("messageCreate", message => {
-	if (message.content === 'ping') {
-		message.author.send("pong");
+client.on("messageCreate", async message => {
+	const { content } = message;
+	const contentSplited = content.split(' ');
+	const commandHandler = contentSplited[0] ? commands[contentSplited[0]] : false;
+	
+	if (!commandHandler || message.author.id === client.user.id) return;
+	
+	try {
+		await commandHandler(message);
+	} catch(ex) {
+		console.log('error', ex)
 	}
 })
 
 client.on('ready', _ => {
+	startDatabaseConnection();
 	console.log(`Ganda Bot is online`);
 });
 
-client.login(botToken)
+client.login(botToken);
