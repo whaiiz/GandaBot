2d4a7bcac6f1adefa7fd3761bc5b6189858f8499
Jaime <jaime_reis18@outlook.pt>
2022-07-23 18:13:36 +0000

    Fixed description of commands

--- commands/help.js
+++ commands/help.js
@@ -3,8 +3,8 @@ import { MessageEmbed } from "discord.js";
 const commandsInstructions = new MessageEmbed().setColor('#0099ff')
     .setTitle("Commands")
     .addFields(
-        { name: 'Create Reminder', value: 'create-reminder dd/mm/aaaa' },
-        { name: 'Create Annual Reminder', value: 'create-annual-reminder dd/mm/' },
+        { name: 'Create Reminder', value: 'create-reminder <description> dd/mm/aaaa' },
+        { name: 'Create Annual Reminder', value: 'create-annual-reminder <description> dd/mm/' },
     );
 
 export const help = message => {
