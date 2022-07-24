36df03083ac03f940fd678dcbbf66c1b5204cf81
Jaime <jaime_reis18@outlook.pt>
2022-07-24 11:53:10 +0000

    Reminder fix

--- commands/help.js
+++ commands/help.js
@@ -3,8 +3,8 @@ import { MessageEmbed } from "discord.js";
 const commandsInstructions = new MessageEmbed().setColor('#0099ff')
     .setTitle("Commands")
     .addFields(
-        { name: 'Create Reminder', value: 'create-reminder <description> dd/mm/aaaa' },
-        { name: 'Create Annual Reminder', value: 'create-annual-reminder <description> dd/mm/' },
+        { name: 'Create Reminder', value: 'create-reminder dd/mm/aaaa <description>' },
+        { name: 'Create Annual Reminder', value: 'create-annual-reminder dd/mm/ <description> ' },
     );
 
 export const help = message => {
